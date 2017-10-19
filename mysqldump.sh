715520e381260df7b943471ff3b2f7f954a25f24
zhangyuchungit <32922532+zhangyuchungit@users.noreply.github.com>
2017-10-19 14:43:11 +0800

    mysqldump.sh
    自动生成多个数据库备份，并删除上次生成的就数据

---
+++ mysqldump.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#此脚本为1week运行一次，每次运行完成之后删除之前的
+#1 1 * * *  /root/script/mysqldump.sh >/root/script/mysqldump.log 2>&1
+#set -x
+#此脚本更改arry组中的数据库名即可,
+array=(chayu_0915)
+now_date=`date +%Y%m%d`
+old_date=`date +%Y%m%d --date="-1 week"`
+order_mysql="/usr/bin/mysqldump -uroot -pbetchayuttl -h192.168.40.241 -B --single-transaction"
+dir_mysql="/data/mysql/backup"
+echo "当前时间:$now_date"
+[ -d $mysql_dir ] &&{
+   /bin/mkdir -p $dir_mysql	
+}
+for i in ${array[@]}
+  do
+  ${order_mysql} $i |gzip > $dir_mysql/${i}_${now_date}.sql.gz;
+  /bin/rm -rf $dir_mysql/${i}_${old_date}.sql.gz
+done
+
